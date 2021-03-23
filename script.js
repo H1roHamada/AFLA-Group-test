1e99898c3979f03e81d9e8684248e938d37758bc
H1roHamada <66208615+H1roHamada@users.noreply.github.com>
2021-03-23 21:42:20 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const PLAN_ITEM = document.querySelectorAll('.plan__item')
 const NUM_LICENSE = document.querySelector('.license-quantity_value')
 const TOTAL_PRICE = document.getElementById('total-price')
 const SELECTED_PLAN = document.getElementById('selected-plan')
-var price = 13;
-var num = 1;
+let price = 13;
+let num = 1;
 
 function multiplyLicensePrice() {
     price = parseInt(price)
@@ -11,9 +11,9 @@ function multiplyLicensePrice() {
     TOTAL_PRICE.innerHTML = `$${num * price}`
 }
 
-PLAN_ITEM.forEach(input => input.addEventListener('click', e => {
+PLAN_ITEM.forEach(plan => plan.addEventListener('click', e => {
     PLAN_ITEM.forEach(e => e.classList.remove('active__plan'))
-    input.classList.add('active__plan')
+    plan.classList.add('active__plan')
     price = e.target.dataset.price
     multiplyLicensePrice()
     SELECTED_PLAN.innerHTML = e.target.dataset.id
